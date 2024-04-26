91860310477c4ca8122797024a92c8166860ba35
kcallaway4 <112517658+kcallaway4@users.noreply.github.com>
2024-04-25 19:12:11 -0600

    k

--- indstudy/test.js
+++ indstudy/test.js
@@ -1,11 +1,15 @@
 const container = d3.select('#scrolly-side');
 const stepSel = container.selectAll('.step');
+let imageList = ['indimg/Finger_model01.JPG', 'indimg/Finger_model02.JPG', 'indimg/misprints4.JPG', 'indimg/finger_model04.2.JPG', 'indimg/finger_model03.JPG','indimg/motor_attach01.JPG','indimg/finger_model05.2.JPG','indimg/hand_attach01.JPG','indimg/hand_attach02.JPG','indimg/hand_attach03.2.JPG', 'indimg/hand_attach04.2.JPG']
+// let image2List = ['indimg/motor_attach01.JPG','indimg/finger_model05.JPG']
 
 function updateChart(index) {
   const sel = container.select(`[data-index='${index}']`);
   const width = sel.attr('data-width');
+  const ind = sel.attr('data-index');
   stepSel.classed('is-active', (d, i) => i === index);
-  container.select('.bar-inner').style('width', width);
+  container.select('.photo').attr('src', imageList[index]);
+//   container.select('.photo2').attr('src', image2List[index]);
 }
 
 function init() {
